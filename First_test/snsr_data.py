2647ca8f6940750f0984baa45f76dfee1113708f
boonsita0808 <60335095+boonsita0808@users.noreply.github.com>
2020-01-28 01:03:40 +0700

    Update snsr_data.py

--- First_test/snsr_data.py
+++ First_test/snsr_data.py
@@ -1,6 +1,10 @@
 import pandas as pd 
 import numpy as np
 
+#for midddle
+#rawdata = data = pd.read_csv("Test1/Middle.csv") 
+#data = pd.read_csv("Test1/Middle.csv",header=0, index_col=0, squeeze=True) 
+
 rawdata = data = pd.read_csv("Test1/1_1.csv") 
 data = pd.read_csv("Test1/1_1.csv",header=0, index_col=0, squeeze=True) 
 
@@ -18,10 +22,19 @@ rm_diff.plot(linewidth=0.5, title = "rolling mean data diff")
 data_diff2 = data_diff.diff()
 data_diff2.plot(linewidth=0.5)
 
-impact = data.iloc[305:355]
-for i, j in impact.iteritems(): 
-    if i != "time":
-        snsr = j
-        snsr_a = np.array(j).transpose()
-        area = np.trapz(snsr_a)
-        print(i,"area",area)
+#for middle 
+#timelist = ["95:145", "185:235", "285:335", "375:420", "470:520", "560:610", "655:705", "735:785", "820:870", "910:960","1005:1055","1120:1170"]
+
+timelist = ["305:355", "410:460", "500:550", "600:650", "695:745", "795:845", "890:940", "1000:1050", "1090:1140", "1185:1235"]
+for i in timelist:
+    print(i)
+    timelist_str = i.split(':')
+    a = int(timelist_str[0])
+    b = int(timelist_str[1])
+    impact = data.iloc[a:b]
+    for colname, j in impact.iteritems(): 
+        if colname != "time":
+            snsr = j
+            snsr_a = np.array(j).transpose()
+            area = np.trapz(snsr_a)
+            print(colname,"area",area)
