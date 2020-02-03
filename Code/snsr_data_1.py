33e202d6e9f95bef1e5b40d27c2864f1acd7bac6
fafaze <52685718+fafaze@users.noreply.github.com>
2020-02-03 17:19:43 +0700

    Update snsr_data_1.py

--- Code/snsr_data_1.py
+++ Code/snsr_data_1.py
@@ -7,8 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
-filelocation = 'E:/Work/Senior Project/Data/Second_test/2_1.csv'
-csvfile = '2_1-summary.csv'
+filelocation = 'E:/Work/Senior Project/Data/Second_test/1_1.csv'
+csvfile = '1_1-summary.csv'
 rawdata = pd.read_csv(filelocation) 
 data = pd.read_csv(filelocation, header=0, index_col=0, squeeze=True) 
 snsr = data[['snsr_11','snsr_12','snsr_21','snsr_22','interupt']]
@@ -34,6 +34,8 @@ for i in range(1,j+1):
     impact_name.append(f'Impact {i}')
 d = {}
 d = {name: snsr.loc[snsr.interupt == impact_name.index(name)+1] for name in impact_name}
+predict = []
+accuracy = 0
 
 with open(csvfile, 'w', newline ='') as f:
     writer = csv.writer(f)
@@ -41,6 +43,7 @@ with open(csvfile, 'w', newline ='') as f:
 for name in impact_name:
     df = d[name].drop(['interupt'], axis=1)
     df_diff = df.diff().fillna(0)
+    predict.append(df_diff.iloc[1].idxmin())
     fig, axs = plt.subplots(2)
     fig.suptitle(name)
     axs[0].plot(df)
@@ -50,7 +53,7 @@ for name in impact_name:
     plt.legend(df.columns, loc='upper center', bbox_to_anchor=(0.5, -0.1), ncol=4)
     figManager = plt.get_current_fig_manager()
     figManager.window.showMaximized()
-    fig.savefig('2_1 ' + str(name) + '.png')
+    fig.savefig('1_1 ' + str(name) + '.png')
     
     for colname, j in df.iteritems(): 
 
@@ -75,3 +78,8 @@ for name in impact_name:
         with open(csvfile, 'a', newline ='') as f:
             writer = csv.writer(f)
             writer.writerow([name, colname, area, area_abs, first, second, first_abs, second_abs, first_diff, second_diff, first_diff_abs, second_diff_abs, second_diff_abs/first_diff_abs])
+for i in range(len(predict)):
+    if predict[i] == 'snsr_11':
+        accuracy += 1
+print('Accuracy of closest sensor prediction = ' + str(accuracy/len(predict)))
+    
