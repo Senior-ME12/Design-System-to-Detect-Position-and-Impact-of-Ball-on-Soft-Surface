ea5382e49cd2cfbc6aaefef415e0e0e79b97f8a8
boonsita0808 <60335095+boonsita0808@users.noreply.github.com>
2020-03-30 21:12:39 +0700

    Update analyze3.py

--- analyze3.py
+++ analyze3.py
@@ -5,15 +5,15 @@ import os
 from scipy import stats
 import matplotlib.pyplot as plt
 #from  matplotlib.ticker import FuncFormatter
-
-INPUT1 = 'all' # file name '1-1' or 'all'
-INPUT2 = 'all' # impact number 'all' or list ex. [1,2,3,5]
+FOLDER = 'Tennis_ball_test_level1'
+FILE = 'all' # file name '1-1.csv' or 'all'
+IMPACT = 'all' # impact number 'all' or list ex. [1,2,3,5]
 PLOT = False
 LINE = False
 BAR = True
 
-def RawData(fileloc):
-
+def RawData(folder,file):
+    fileloc = folder+'/'+file
     df = pd.read_csv(fileloc, index_col = 'time')
     df = df.drop(['interupt'], axis=1).dropna()
     return df
@@ -25,7 +25,7 @@ def NormalData(raw, plot = True):
     return df
 
 def PlotRN(raw,normal):
-    sns.set()
+    sns.set_style("whitegrid")
     plt.subplot(2,1,1)
     raw.plot(title = 'Raw Data', colormap = 'tab20',ax=plt.gca())
     plt.ylabel(f'acceleration (m/s\N{SUPERSCRIPT TWO})')
@@ -73,6 +73,8 @@ def FilterImpact(normalwithimpact,impactlist,N,line = True, bar = True):
             plt.figure()
             df1 = df.stack().reset_index().rename({'level_2': 'snsr_name',0:'snsr_value'}, axis=1)
             
+            sns.set_style("whitegrid")
+            
             ymin, ymax = min(df1.snsr_value), max(df1.snsr_value)
             for i in range(16):
                 plt.subplot(4,4,i+1)
@@ -83,62 +85,82 @@ def FilterImpact(normalwithimpact,impactlist,N,line = True, bar = True):
                 plt.xticks(fontsize=10)
                 plt.yticks(fontsize=10)
                 plt.legend().set_visible(False)
-            plt.suptitle(f"Acceleration (m/s\N{SUPERSCRIPT TWO}) - Time (ms) on sensor {INPUT1}", fontsize=16)
+            plt.suptitle(f"Acceleration (m/s\N{SUPERSCRIPT TWO}) - Time (ms) on sensor {FILE.split('.')[0]} ({FOLDER})", fontsize=16)
             plt.subplots_adjust(top=0.920, bottom=0.04, left=0.04, right=0.995, hspace=0.2, wspace=0.2)
             
         #plot area
-        if bar == True:
-            plt.figure()
-            dfarea = pd.DataFrame(columns=['impact','snsr_11','snsr_12','snsr_13','snsr_14',
+        dfarea = pd.DataFrame(columns=['impact','snsr_11','snsr_12','snsr_13','snsr_14',
                                            'snsr_21','snsr_22','snsr_23','snsr_24',
                                            'snsr_31','snsr_32','snsr_33','snsr_34',
                                            'snsr_41','snsr_42','snsr_43','snsr_44'])
-            for j in impactlist:
-                df3 = df.query('impact == @j')
-                area = [j] + list(np.trapz(df3.abs(), axis=0))
-                area = pd.Series(area, index = dfarea.columns)
-                dfarea = dfarea.append(area, ignore_index=True)
-            dfarea = dfarea.set_index('impact').stack().reset_index().rename({'level_1': 'snsr_name',0:'snsr_area'}, axis=1)
+        for j in impactlist:
+            df3 = df.query('impact == @j')
+            area = [j] + list(np.trapz(df3.abs(), axis=0))
+            area = pd.Series(area, index = dfarea.columns)
+            dfarea = dfarea.append(area, ignore_index=True)
+        dfarea = dfarea.set_index('impact').stack().reset_index().rename({'level_1': 'snsr_name',0:'snsr_area'}, axis=1)
 #            dfarea.impact = dfarea.impact.astype(int).astype(str)
+        dfareasum = dfarea[['impact','snsr_area']].groupby('impact').agg({'snsr_area':np.sum})
+        if bar == True:
+            
+            plt.figure()
+            sns.set_style("whitegrid")
             ymin, ymax = min(dfarea.snsr_area), max(dfarea.snsr_area)
             for i in range(16):
                 plt.subplot(4,4,i+1)
                 df4 = dfarea[dfarea['snsr_name'] == columns[i]]
-                sns.barplot(data=df4, x=[int(x) for x in impactlist], y='snsr_area').set_ylim([ymin,ymax])
+                sns.barplot(data=df4, x=[int(x) for x in impactlist], y='snsr_area').set_ylim([0,ymax])
                 plt.xlabel('')
                 plt.ylabel('')
             
     #            plt.legend().set_visible(False)
     #            plt.yaxis.set_major_locator(MaxNLocator(integer=True))
     #            plt.gca().xaxis.set_major_formatter(FuncFormatter(lambda x, _: int(x)))
-            plt.suptitle(f"Area (mm/s) on sensor {INPUT1}", fontsize=16)
+            plt.suptitle(f"Area (mm/s) on sensor {FILE.split('.')[0]} ({FOLDER})", fontsize=16)
             plt.subplots_adjust(top=0.945, bottom=0.04, left=0.04, right=0.995, hspace=0.2, wspace=0.2)
+            
+           
+            plt.figure()
+            sns.set_style("whitegrid")
+            sns.barplot(data=dfareasum, x=[int(x) for x in dfareasum.index], y='snsr_area')
+            plt.xlabel('')
+            plt.ylabel('')
+    #            plt.legend().set_visible(False)
+    #            plt.yaxis.set_major_locator(MaxNLocator(integer=True))
+    #            plt.gca().xaxis.set_major_formatter(FuncFormatter(lambda x, _: int(x)))
+            plt.suptitle(f"Sum Area (mm/s) on sensor {FILE.split('.')[0]} ({FOLDER})", fontsize=16)
+            plt.subplots_adjust(top=0.945, bottom=0.04, left=0.04, right=0.995, hspace=0.2, wspace=0.2)
+            
     #        plt.savefig(f"{key}.png", bbox_inches='tight')
     else :
         df = print('Argument error')  
         dfarea = print('Argument error')  
-    return df, dfarea
+        dfareasum = print('Argument error')  
+    return df, dfarea, dfareasum
 
-if INPUT1 == 'all':
+if FILE == 'all':
     frames = {}
-    directory = r'Tennis_ball_test'
+    directory = FOLDER
     for filename in os.listdir(directory):  # or filesname in ['1-1.csv','1-2.csv','1-3.csv','1-4.csv','2-1.csv','2-2.csv', '2-3.csv', '2-4.csv', '3-1.csv', '3-2.csv', '3-3.csv', '3-4.csv', '4-1.csv', '4-2.csv', '4-4.csv',]
         if filename.endswith(".csv"):
+            FILE = filename
             name = filename.split('.')[0]
-            INPUT1 = name
+#            INPUT1 = name
             frames[name] = {}
-            df1 = RawData('Tennis_ball_test/'+name+'.csv')
+            df1 = RawData(FOLDER,FILE)
             df2 = NormalData(df1,plot = PLOT)           
             df3,n = FindImpact(df2)
-            df4,area = FilterImpact(df3, INPUT2, n, line = LINE, bar = BAR) 
+            df4,area,areasum = FilterImpact(df3, IMPACT, n, line = LINE, bar = BAR) 
             frames[name]['raw'] = df1
             frames[name]['normalized'] = df2
             frames[name]['impact'] = df3
             frames[name]['area'] = area
-    del area,df1,df2,df3,df4,directory,filename,n,name
+            frames[name]['areasum'] = areasum
+    locals()[FOLDER] = frames
+    del area,df1,df2,df3,df4,directory,filename,n,name,frames,areasum
     
 else :
-    raw = RawData('Tennis_ball_test/'+INPUT1+'.csv')
-    normalized = NormalData(raw,plot = PLOT)           
-    impact,n = FindImpact(normalized)
-    impactfilter,area = FilterImpact(impact, INPUT2, n, line = LINE, bar = BAR)
+    df1 = RawData(FOLDER,FILE)
+    df2 = NormalData(df1,plot = PLOT)           
+    df3,n = FindImpact(df2)
+    df4,area,areasum = FilterImpact(df3, IMPACT, n, line = LINE, bar = BAR)
